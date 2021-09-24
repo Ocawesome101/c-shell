393402aeafd03207f41fa1398d25cf6a11611236
Ocawesome101 <ocawesome101@gmail.com>
2021-09-24 15:55:40 -0400

    pipes mostly work, excepting that piping into `cat` doesn't behave correctly

--- main.c
+++ main.c
@@ -26,9 +26,10 @@ int exec_and_wait(int input, int output, const char *file, char *const argv[]) {
       arg = argv[1];
     }
     chdir(argv[1]);
-    char abuf[4096];
-    getcwd(&abuf, 4096);
+    char* abuf = malloc(4096);
+    getcwd(abuf, 4096);
     setenv("PWD", abuf, 1);
+    free(abuf);
     return 0;
   } else if (strcmp(file, "exit") == 0) {
     exit(0);
@@ -39,10 +40,12 @@ int exec_and_wait(int input, int output, const char *file, char *const argv[]) {
         break;
   
       case 0: /* Child */
-        int c1 = dup2(input, STDIN_FILENO);
-        int c2 = dup2(output, STDOUT_FILENO);
+        dup2(input, STDIN_FILENO);
+        dup2(output, STDOUT_FILENO);
         setvbuf(stdout, NULL, _IONBF, 0);
         execvp(file, argv);
+        close(input);
+        close(output);
   
       default: /* Parent */
         int stat_loc;
@@ -61,13 +64,15 @@ struct command {
 };
 
 int exec_vect(char *const argv[]) {
-  struct command* commands[sizeof(struct command) * MAX_WORDS];
+  struct command* commands[(MAX_WORDS + 1) * sizeof(int*)];
   int i = 0;
   int c = 0;
   int a = 0;
+  int n = 0;
   struct command cmd;
   cmd.input = STDIN_FILENO;
   cmd.output = STDOUT_FILENO;
+  cmd.argv[0] = NULL;
   commands[c] = &cmd;
   for (i = 0; argv[i] != NULL; i++) {
     if (strcmp(argv[i], "|") == 0) {
@@ -81,16 +86,14 @@ int exec_vect(char *const argv[]) {
       commands[c] = &cmd;
       commands[c]->input = pipes[0];
       commands[c]->output = STDOUT_FILENO;
+      commands[c]->argv[0] = NULL;
     } else {
       commands[c]->argv[a] = malloc(MAX_WORD_LENGTH);
-      int n;
-      for (n = 0; argv[i][n] != '\0'; n++) {
-        commands[c]->argv[a][n] = argv[i][n];
-      }
-      commands[c]->argv[a][n] = '\0';
+      strcpy(commands[c]->argv[a], argv[i]);
       a++;
     }
   }
+  commands[c]->argv[a] = NULL;
   c++;
   commands[c] = NULL;
 
@@ -100,20 +103,19 @@ int exec_vect(char *const argv[]) {
       exec_and_wait(commands[i]->input, commands[i]->output,
           commands[i]->argv[0], commands[i]->argv);
       // clean up
-      int n;
       for (n = 0; commands[i]->argv[n] != NULL; n++) {
         free(commands[i]->argv[n]);
       }
     }
   }
+  return 0;
 }
 
 int main() {
   char hostname[128];
   gethostname(hostname, 128);
   while (1) {
-    printf("%s@%s: %s> ", getenv("USER"), hostname, basename(getenv("PWD")));
-    char* str = readline(NULL);
+    char* str = readline("$ ");
     if (str == NULL) {
       exit(0);
     }
@@ -127,18 +129,9 @@ int main() {
       int word = 0;
       argv[0] = malloc(MAX_WORD_LENGTH);
       for (i = 0; i < len; i++) {
-        if (str[i] == ' ' && alen > 0) {
+        if (str[i] == ' ' && alen > 0 && (i + 1) < len) {
           argv[word][alen] = '\0';
-
-          /*if (word == 0) {
-            argv[1] = malloc(MAX_WORD_LENGTH);
-            int n;
-            for (n = 0; n <= alen; n++) {
-              argv[1][n] = argv[0][n];
-            }
-            word += 1;
-          }*/
-
+          
           word += 1;
           alen = 0;
           argv[word] = malloc(MAX_WORD_LENGTH);
